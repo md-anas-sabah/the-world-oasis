a985795aaba8228a9d2683ba798962f7b106f427
Md Anas Sabah <mdanassabah@gmail.com>
2023-07-15 23:25:28 +0530

    update: integrated react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import PageNotFound from "./pages/PageNotFound";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="bookings" element={<Bookings />} />
+        <Route path="cabins" element={<Cabins />} />
+        <Route path="users" element={<Users />} />
+        <Route path="settings" element={<Settings />} />
+        <Route path="account" element={<Account />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
+
+// import { styled } from "styled-components";
+// import GlobalStyles from "./styles/GlobalStyles";
+// import Button from "./ui/Button";
+// import Input from "./ui/Input";
+// import Heading from "./ui/Heading";
+// import Row from "./ui/Row";
+
+// const StyledApp = styled.div`
+//   /* background-color: orangered; */
+//   padding: 20px;
+// `;
+
+// function App() {
+//   return (
+//     <>
+//       <GlobalStyles />
+//       <StyledApp>
+//         <Row>
+//           <Row type="horizontal">
+//             <Heading as="h1">The Wild Oasis</Heading>
+//             <div>
+//               <Heading as="h2">Check In and Out</Heading>
+//               <Button onClick={() => alert("User checked in")}>Check In</Button>
+//               <Button
+//                 variations="secondary"
+//                 sizes="small"
+//                 onClick={() => alert("User checked out")}
+//               >
+//                 Check Out
+//               </Button>
+//             </div>
+//           </Row>
+//           <Row>
+//             <Heading as="h3">Form</Heading>
+//             <form>
+//               <Input type="number" placeholder="Number of guests" />
+//               <Input type="number" placeholder="Number of guests" />
+//             </form>
+//           </Row>
+//         </Row>
+//       </StyledApp>
+//     </>
+//   );
+// }
+
+// export default App;
