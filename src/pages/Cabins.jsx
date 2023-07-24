101b7c77d4dea61bdec72e0b07dfa42a8ccf4c4f
Md Anas Sabah <mdanassabah@gmail.com>
2023-07-24 16:30:53 +0530

    update: added modal window

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -7,9 +7,9 @@ import CabinTable from "../features/cabins/CabinTable";
 import Button from "../ui/Button";
 import { useState } from "react";
 import CreateCabinForm from "../features/cabins/CreateCabinForm";
+import AddCabin from "../features/cabins/AddCabin";
 
 function Cabins() {
-  const [showForm, setShowForm] = useState(false);
   return (
     <>
       <Row type="horizontal">
@@ -19,10 +19,7 @@ function Cabins() {
 
       <Row>
         <CabinTable />
-        <Button onClick={() => setShowForm((show) => !show)}>
-          Add New Cabin
-        </Button>
-        {showForm && <CreateCabinForm />}
+        <AddCabin />
       </Row>
     </>
   );
